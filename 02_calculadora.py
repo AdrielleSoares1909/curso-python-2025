2ca9e5a99e644846ae6a8e09aca8bb8b7ab70a70
Adrielle Cristian <dricalove1@hotmail.com>
2025-10-23 14:45:41 -0300

    operadores matematicos basicos

---
+++ 02_calculadora.py
@@ -0,0 +1,20 @@
+#soma
+print("1 + 1 =", 1 + 1)
+
+#subtracao
+print("5 - 3 =", 5 - 3)
+
+#multiplicacao
+print("1 x 4 =", 1 * 4)
+
+#divisao
+print("1 / 4 =", 1 / 4)
+
+#divisao com inteira parte
+print("10 // 4 =", 10 // 4)
+
+#resto da divisao
+print("10 % 4 =", 10 % 4)
+
+#potencia
+print("3 ** 4 =", 3 ** 4)
