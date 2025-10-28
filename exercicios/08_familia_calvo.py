f0933db146e7f42aa5aad1453fa5a279c0b71f8c
Adrielle Cristian <dricalove1@hotmail.com>
2025-10-28 08:53:09 -0300

    resolvendo exercicios

---
+++ exercicios/08_familia_calvo.py
@@ -0,0 +1,13 @@
+texto = """Familia calvo
+(1) familia calvo 
+(2) familia silva
+"""
+
+opcao = input(texto)
+
+if opcao == "1":
+    print("Voce pertence a familia Calvo!")
+elif opcao == "2":
+    print("voce pertence a familia Silva!")
+else:
+    print("Voce não pertence a familia real!")
