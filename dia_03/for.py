c452c1e3beaf88f71d740215585af19bd1f35282
Adrielle Cristian <dricalove1@hotmail.com>
2025-10-28 12:55:37 -0300

    conceitos do for

---
+++ dia_03/for.py
@@ -0,0 +1,19 @@
+nome = "Adrielle Cristian"
+
+for letra in nome:
+    print(letra)
+
+#exemplos
+numero = 2
+max_numero = 100
+
+for i in range(1, max_numero+1):
+    print(numero, "x", i, "=", numero * i)
+
+#exemplos
+
+for numero in range(4, 101):
+    if numero % 4 == 0:
+        print(numero)
+
+
