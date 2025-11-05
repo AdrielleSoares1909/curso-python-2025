137a8399e69644d2911b90798ef2f6ba326853d0
Adrielle Cristian <dricalove1@hotmail.com>
2025-11-05 07:42:40 -0300

    Biblioteca/Modulos em python

---
+++ dia_06/08_modulos_lib.py
@@ -0,0 +1,21 @@
+# IMPORTANDO BIBLIOTECA / MODULOS
+
+import math # Importa toda a biblioteca matematica 
+from  math import pi # importa somente o pi da biblioteca
+import math as mh # renomeia a biblioteca de math para mh (podendo usar os dois)
+
+
+print(math.sqrt(9))
+
+print(math.pow(9, 4))
+
+print(mh.pow(9, 4))
+
+print(math.pi)
+
+print(math.e)
+
+print(mh.e)
+
+
+
---
+++ dia_06/09_aleatorio.py
@@ -0,0 +1,12 @@
+# Biblioteca de escolhas Aleatorios Random
+
+
+import random # random = gerar escolhas aleatorias
+
+print(random.randint(1, 10)) # Seleciona um numero aleatoria de 1 a 10
+
+
+
+x = ["drica", "Maria", "Ana", "jose"]
+
+print(random.choice(x)) # Seleciona o nome aleatoriamente da lista
