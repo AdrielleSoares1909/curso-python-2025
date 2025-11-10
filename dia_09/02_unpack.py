aa1bcccc6fe747b4b470ca73083dfcf589d56397
Adrielle Cristian <dricalove1@hotmail.com>
2025-11-10 10:34:49 -0300

    Mais sobre Listas

---
+++ dia_09/02_unpack.py
@@ -0,0 +1,14 @@
+a = 1
+b = 5 
+
+a,b = b,a
+
+print(a)
+print(b)
+
+a, b, *resto = 1, 2, 3, 4 , 55, 66
+
+print(a, b, resto) 
+
+# args = segue o mesmo padrao.
+# unpack = pegar um interavel e descomprimir a tupla em um ou mais elementos
